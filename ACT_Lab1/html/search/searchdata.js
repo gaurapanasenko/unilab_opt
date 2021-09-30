52c9a854ee9efeab7b1add4a732da0dfd4b45599
Yehor Panasenko <yehor.panasenko@gmail.com>
2021-09-30 04:58:42 +0300

    ACT_Lab1

---
+++ ACT_Lab1/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "mÐ",
+  1: "m",
+  2: "Ð"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Всі",
+  1: "Файли",
+  2: "Сторінки"
+};
+
